b94eaf045b831d9ad984d361266688c4fd36aa01
khaledk8 <khalidkari1@gmail.com>
2023-09-22 20:25:12 +0300

    Finished most of the script backspace is bugged

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let smallVar = ''
 let bigVar = ''
 let arrayStorage = []
-let calculationResult
+let calculationResult = ''
 
 const elementArray = document.querySelectorAll(".digit")
 const operatorArray = document.querySelectorAll(".operator.last")
@@ -9,6 +9,7 @@ const negativePositive = document.querySelector(".neg-pos")
 const result = document.getElementById("equal")
 const operations = document.getElementById("operations")
 const backSpace= document.getElementById("delete")
+const resultDiv = document.getElementById("result")
 
 
 
@@ -42,10 +43,34 @@ negativePositive.addEventListener('click', () => {
 })
 
 result.addEventListener('click', () => {
-    if (smallVar == '') alert("error")
+    if (smallVar == '') {
+        resultDiv.textContent = "Error"
+        return
+    }
     arrayStorage.push(bigVar)
+    bigVar = ''
+    calculate()
 
 })
+
+backSpace.addEventListener('click', () => {
+    if (arrayStorage[arrayStorage.length - 1] == "") arrayStorage.pop()
+    operations.textContent = operations.textContent.slice(0,-1)
+    if (bigVar == "" && smallVar == "") {
+        arrayStorage.pop()
+        return
+    }
+    arrayStorage[arrayStorage.length - 1] = arrayStorage[arrayStorage.length - 1].slice(0,-1)
+
+})
+
+/* function checkOperations () {
+    if (arrayStorage[arrayStorage.length - 1] == '+') return true;
+    if (arrayStorage[arrayStorage.length - 1] == 'x') return true;
+    if (arrayStorage[arrayStorage.length - 1] == '-') return true;
+    if (arrayStorage[arrayStorage.length - 1] == '/') return true;
+    return false;
+} */
 
 function add (a,b) {
     return a+b
@@ -65,5 +90,54 @@ function divide (a,b) {
 
 
 function calculate () {
+    let filtered = arrayStorage.filter((element) => {
+        if (element=='x') return true;
+        if (element=='/') return true;
+    })
+    for (let i = 0; i <= filtered.length; i++) {
+        if (arrayStorage.includes('x') || arrayStorage.includes('/')) firstOrderCalculate()
+    }    
+    if (arrayStorage.length < 3) {
+        resultDiv.textContent =  Math.round(parseFloat(arrayStorage[0]) * 100) / 100
+        return
+    }    
+    console.log(arrayStorage.length)
+    secondOrderCalculate()
+    if (arrayStorage.length > 1) calculate()
+    resultDiv.textContent = Math.round(parseFloat(arrayStorage[0]) * 100) / 100
 
+}
+
+
+function firstOrderCalculate () {
+    if (arrayStorage.includes('x') && arrayStorage.includes('/')) {
+        if (arrayStorage.indexOf('x') < arrayStorage.indexOf('/')) {
+            arrayStorage.splice(arrayStorage.indexOf('x')-1,3,multiply(parseFloat(arrayStorage[arrayStorage.indexOf('x')-1]),parseFloat(arrayStorage[arrayStorage.indexOf('x')+1])))
+        } else {
+            arrayStorage.splice(arrayStorage.indexOf('/')-1,3,divide(parseFloat(arrayStorage[arrayStorage.indexOf('/')-1]),parseFloat(arrayStorage[arrayStorage.indexOf('/')+1])))
+        }
+        return
+    }
+    if (arrayStorage.includes('x')) {
+        arrayStorage.splice(arrayStorage.indexOf('x')-1,3,multiply(parseFloat(arrayStorage[arrayStorage.indexOf('x')-1]),parseFloat(arrayStorage[arrayStorage.indexOf('x')+1])))
+        return
+    }
+    arrayStorage.splice(arrayStorage.indexOf('/')-1,3,divide(parseFloat(arrayStorage[arrayStorage.indexOf('/')-1]),parseFloat(arrayStorage[arrayStorage.indexOf('/')+1])))
+
+}
+
+function secondOrderCalculate () {
+    if (arrayStorage.includes('+') && arrayStorage.includes('-')) {
+        if (arrayStorage.indexOf('+') < arrayStorage.indexOf('-')) {
+            arrayStorage.splice(arrayStorage.indexOf('+')-1,3,add(parseFloat(arrayStorage[arrayStorage.indexOf('+')-1]),parseFloat(arrayStorage[arrayStorage.indexOf('+')+1])))
+        } else {
+            arrayStorage.splice(arrayStorage.indexOf('-')-1,3,subtract(parseFloat(arrayStorage[arrayStorage.indexOf('-')-1]),parseFloat(arrayStorage[arrayStorage.indexOf('-')+1])))
+        }
+        return
+    }
+    if (arrayStorage.includes('+')) {
+        arrayStorage.splice(arrayStorage.indexOf('+')-1,3,add(parseFloat(arrayStorage[arrayStorage.indexOf('+')-1]),parseFloat(arrayStorage[arrayStorage.indexOf('+')+1])))
+        return
+    }
+    arrayStorage.splice(arrayStorage.indexOf('-')-1,3,subtract(parseFloat(arrayStorage[arrayStorage.indexOf('-')-1]),parseFloat(arrayStorage[arrayStorage.indexOf('-')+1])))
 }
